d1695a4ce590d968764577bfed70c1a2f08a082d
LuizAdamchuk <adis_jr@hotmail.com>
2020-06-18 20:38:21 -0300

    Funny Discord Clone build

---
+++ build/precache-manifest.4233ae98a61a22a1f37ae982eb0dc8b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e1ee809dcbe6dbcdc05e2eb267b878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573f624e26e63a53409f",
+    "url": "/static/js/2.56715771.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.56715771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18d804f7be18d94f938",
+    "url": "/static/js/main.c1b8440a.chunk.js"
+  },
+  {
+    "revision": "d7a625251aba42204f95",
+    "url": "/static/js/runtime-main.02d529e8.js"
+  },
+  {
+    "revision": "c42ff9e3ae0e5407b4672e487a96adc7",
+    "url": "/static/media/logo.c42ff9e3.svg"
+  }
+]);
